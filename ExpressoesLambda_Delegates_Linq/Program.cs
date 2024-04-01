81ba681df1efd3444666e3505b2d00d1d571ea88
Augusto Omena <augusto.omena.n@gmail.com>
2024-04-01 18:44:25 -0300

    Adicionar arquivos de projeto.

---
+++ ExpressoesLambda_Delegates_Linq/Program.cs
@@ -0,0 +1,6 @@
+﻿/*Problema Exemplo
+ * Fazer um programa que, a partir de uma lista de produtos, remova da
+ * lista apenas aqueles cujo o preço mínimo seja 100.
+ */
+
+Console.WriteLine("Hello, World!");
