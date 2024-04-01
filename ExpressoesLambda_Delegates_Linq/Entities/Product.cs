a88b9ee090550624ba861b08dc2a42dcae4a60c1
Augusto Omena <augusto.omena.n@gmail.com>
2024-04-01 20:06:57 -0300

    Predicate (exemple with RemoveAll)

---
+++ ExpressoesLambda_Delegates_Linq/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace ExpressoesLambda_Delegates_Linq.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string Name, double price)
+        {
+            Price = price;
+            this.Name = Name;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2");
+        }
+    }
+}
--- ExpressoesLambda_Delegates_Linq/Program.cs
+++ ExpressoesLambda_Delegates_Linq/Program.cs
@@ -1,6 +1,38 @@
-﻿/*Problema Exemplo
- * Fazer um programa que, a partir de uma lista de produtos, remova da
- * lista apenas aqueles cujo o preço mínimo seja 100.
- */
+﻿using System;
+using System.Collections.Generic;
+using ExpressoesLambda_Delegates_Linq.Entities;
 
-Console.WriteLine("Hello, World!");
+namespace ExpressoesLambda_Delegates_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.00));
+
+            list.RemoveAll(p => p.Price >= 100.00);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+            // ou
+
+            list.RemoveAll(ProductTest);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.00;
+        }
+    }
+}
